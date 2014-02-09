d3d57f818f3d236b33f1d5b511e9f87105ccc161
Roy Zheng <roycraft3@gmail.com>
2014-02-09 13:51:06 -0800

    minor

--- BlindSwordmaster/blindswordmaster/src/main/java/com/wontonst/blindswordmaster/game/model/PlayerState.java
+++ BlindSwordmaster/blindswordmaster/src/main/java/com/wontonst/blindswordmaster/game/model/PlayerState.java
@@ -3,7 +3,7 @@ package com.wontonst.blindswordmaster.game.model;
 import com.wontonst.blindswordmaster.game.GameComponent;
 
 /**
- * Created by roycr_000 on 2/9/14.
+ * Created by Roy Zheng on 2/9/14.
  */
 public class PlayerState implements GameComponent {
 
