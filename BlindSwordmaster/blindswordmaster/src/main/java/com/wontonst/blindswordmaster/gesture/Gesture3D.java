e19826683c2aa233c1d3c0c8f75273c8e7d59db8
Jack Wong <yamijack2000@gmail.com>
2014-02-09 15:34:24 -0800

    Add 3D gesture detection. Add Guava library.

--- BlindSwordmaster/blindswordmaster/src/main/java/com/wontonst/blindswordmaster/gesture/Gesture3D.java
+++ BlindSwordmaster/blindswordmaster/src/main/java/com/wontonst/blindswordmaster/gesture/Gesture3D.java
@@ -4,5 +4,7 @@ package com.wontonst.blindswordmaster.gesture;
  * Created by jack on 2/8/14.
  */
 public enum Gesture3D {
-    SLASH_LEFT, SLASH_RIGHT, SLASH_UP, SLASH_DOWN
+    SLASH_LEFT, SLASH_RIGHT, SLASH_UP, SLASH_DOWN,
+    BLOCK_LEFT, BLOCK_RIGHT, BLOCK_UP, BLOCK_DOWN,
+    NONE
 }
--- BlindSwordmaster/blindswordmaster/src/main/java/com/wontonst/blindswordmaster/gesture/Gesture3DDetector.java
+++ BlindSwordmaster/blindswordmaster/src/main/java/com/wontonst/blindswordmaster/gesture/Gesture3DDetector.java
@@ -7,49 +7,184 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 
-import com.wontonst.blindswordmaster.game.controller.CommandVerifier;
+import com.google.common.collect.EvictingQueue;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by jack on 2/8/14.
  */
 public class Gesture3DDetector implements SensorEventListener {
-    private static final int RATE = SensorManager.SENSOR_DELAY_GAME;
-
-    CommandVerifier verifier;
+    private static final String TAG = "Gesture3DDetector";
+    private static final int SAMPLING_RATE = SensorManager.SENSOR_DELAY_GAME;
+    private static final float GRAVITY_THRESHOLD = SensorManager.STANDARD_GRAVITY / 2.0f;
+    private static int[] sensorTypes =  { Sensor.TYPE_LINEAR_ACCELERATION,
+            Sensor.TYPE_ROTATION_VECTOR, Sensor.TYPE_GRAVITY };
+    private static final float MOVEMENT_THRESHOLD = 2.0f;
 
     private SensorManager sensorManager;
-    private Sensor linearAccelerationSensor;
-    private Sensor rotationVectorSensor;
+    private Map<Sensor, Integer> sensorAccuracies = new HashMap<Sensor, Integer>(3);
 
-    public Gesture3DDetector(Context context, CommandVerifier verifier) {
-        this.verifier = verifier;
+    private boolean calibrating = false;
+    private float[] referenceMatrix;
+    private int phoneOrientation = 1;
+    private EvictingQueue<Gesture3D> lastMovements = EvictingQueue.create(9);
+
+    public Gesture3DDetector(Context context) {
         sensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
-        linearAccelerationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
 
-        Log.i("Gesture3DDetector", "created");
+        for (int type : sensorTypes) {
+            sensorAccuracies.put(sensorManager.getDefaultSensor(type),
+                    SensorManager.SENSOR_STATUS_UNRELIABLE);
+        }
+        Log.i(TAG, "Created");
     }
 
     public void startDetecting() {
-        Log.i("Gesture3DDetector", "started");
-        sensorManager.registerListener(this, linearAccelerationSensor, RATE);
-        sensorManager.registerListener(this, rotationVectorSensor, RATE);
+        for (Sensor sensor : sensorAccuracies.keySet()) {
+            sensorManager.registerListener(this, sensor, SAMPLING_RATE);
+        }
+        Log.i(TAG, "Detection started");
+    }
+
+    public void calibrate() {
+        calibrating = true;
     }
 
     public void stopDetecting() {
-        Log.i("Gesture3DDetector", "stopped");
         sensorManager.unregisterListener(this);
+        Log.i(TAG, "Detection stopped");
     }
 
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        Log.i("Gesture3DDetector: " + sensorEvent.sensor.getName(), Arrays.toString(sensorEvent.values));
+    public void onSensorChanged(SensorEvent event) {
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_GRAVITY:
+                int newOrientation;
+                if (event.values[2] >= GRAVITY_THRESHOLD) {
+                    newOrientation = 1;
+                } else if (event.values[2] <= -GRAVITY_THRESHOLD) {
+                    newOrientation = -1;
+                } else {
+                    newOrientation = 0;
+                }
+                if (newOrientation != phoneOrientation) {
+                    Log.i(TAG, "Orientation changed: " + newOrientation);
+                    phoneOrientation = newOrientation;
+                }
+                break;
+            case Sensor.TYPE_ROTATION_VECTOR:
+                float[] rotationMatrix = new float[16];
+                SensorManager.getRotationMatrixFromVector(rotationMatrix,
+                        event.values);
+                if (calibrating) {
+                    referenceMatrix = rotationMatrix;
+                    calibrating = false;
+                }
+                break;
+            case Sensor.TYPE_LINEAR_ACCELERATION:
+                float[] a = event.values.clone();
+                Set<Gesture3D> gestures = accelerationsToGesture3Ds(a, phoneOrientation);
+                if (gestures.isEmpty()) {
+                } else {
+                    for (Gesture3D gesture : gestures) {
+                        lastMovements.add(gesture);
+                    }
+                }
+
+                Gesture3D actualGesture = null;
+                if (lastMovements.remainingCapacity() == 0) {
+                    // Make sure lastMovements has all of the same gesture
+                    Gesture3D foo = null;
+                    Iterator<Gesture3D> itr = lastMovements.iterator();
+                    while (itr.hasNext()) {
+                        Gesture3D bar = itr.next();
+                        if (foo == null) {
+                            foo = bar;
+                        } else if (foo != bar) {
+                            break;
+                        } else if (!itr.hasNext()) {
+                            actualGesture = bar;
+                        }
+                    }
+                }
+                if (actualGesture != null && gestures.contains(actualGesture)) {
+                    Log.i(TAG, actualGesture.toString());
+                }
+                break;
+        }
+    }
+
+    private static Set<Gesture3D> accelerationsToGesture3Ds(float[] acceleration, int orientation) {
+        Set<Gesture3D> gestures = new HashSet<Gesture3D>(2);
+        int[] ternary = analogToBalancedTernaryFilter(acceleration, MOVEMENT_THRESHOLD);
+        if (orientation != 0) {
+            // Slash orientation
+            int[] adjusted = scalarMultiplication(orientation, ternary);
+            if (adjusted[0]  > 0) {
+                gestures.add(Gesture3D.SLASH_RIGHT);
+            } else if (ternary[0] < 0) {
+                gestures.add(Gesture3D.SLASH_LEFT);
+            }
+            if (adjusted[1] > 0) {
+                gestures.add(Gesture3D.SLASH_UP);
+            } else if (ternary[1] < 0) {
+                gestures.add(Gesture3D.SLASH_DOWN);
+            }
+        } else {
+            // Block orientation
+            if (ternary[0] > 0) {
+                gestures.add(Gesture3D.BLOCK_RIGHT);
+            } else if (ternary[0] < 0) {
+                gestures.add(Gesture3D.BLOCK_LEFT);
+            }
+            if (ternary[1] > 0) {
+                gestures.add(Gesture3D.BLOCK_UP);
+            } else if (ternary[1] < 0) {
+                gestures.add(Gesture3D.BLOCK_DOWN);
+            }
+        }
+        return gestures;
+    }
+
+    private static int[] scalarMultiplication(int scalar, int[] vector) {
+        int[] product = new int[vector.length];
+        for (int i = 0; i < vector.length; ++i) {
+            product[i] = scalar * vector[i];
+        }
+        return product;
+    }
+
+    private static int[] analogToBalancedTernaryFilter(float[] analog, float threshold) {
+        int[] ternary = new int[analog.length];
+
+        for (int i = 0; i < analog.length; ++i) {
+            if (analog[i] > threshold) {
+                ternary[i] = 1;
+            } else if (analog[i] < -threshold) {
+                ternary[i] = -1;
+            }
+            // else analog[i] is left as 0
+        }
+        return ternary;
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-        // Method intentionally left blank
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        sensorAccuracies.put(sensor, accuracy);
+    }
+
+    private class SensorData {
+        private final Sensor sensor;
+        private int accuracy;
+
+        private SensorData(Sensor sensor) {
+            this.sensor = sensor;
+            accuracy = SensorManager.SENSOR_STATUS_UNRELIABLE;
+        }
     }
 }
